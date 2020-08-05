24a932cd86cc8a2e882f81d403e37248eb23531e
Benzo26 <bugesi.benzo@gmail.com>
2020-08-05 13:08:02 +0300

    add text, and buttons for location and delivery request

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 (document).ready(function(){
     $("table").hide();
     $(".finalorder-buttons").hide();
-    $(".additional-info").hide();
+    $(".prompt-info").hide();
     $(".btn.yes").hide();
     $(".btn.no").hide();
-    $(".additional-info h4").hide();
-  
+    $(".prompt-info h4").hide();
+
+    $('.btn.order').click(function() {
+        var sizeOfPizza = $(".size option:selected").val();
+        var toppingsOfPizza = $(".toppings option:selected").val();
+        var crustOfPizza = $(".crust option:selected").val();
+        var total = parseInt(sizeOfPizza) + parseInt(toppingsOfPizza) + parseInt(crustOfPizza);
+        var order = 1;
+        var grandTotal = 0;
+    
+    $("table").show();
+    $(".finalorder-buttons").show();
+    $(".btn.order").hide(); 
+    
+    }
